c65690c4fad88eb33348b1a40b7452d4a76be751
Nobeen <n.islam.1@outlook.com>
2022-04-16 13:16:25 +0100

    removed selectRemakes query

--- src/searchMoviesCLI.ts
+++ src/searchMoviesCLI.ts
@@ -12,25 +12,6 @@ select * from movies
 limit 1
 
 `;
-const selectRemakes = `
-select 
-	movies_1.id as original_movie_id,
-    movies_1.name as original_name,
-    movies_1.date as original_date,
-    m_ref.movie_id as remake_movie_id,
-    movies_2.name as remake_name,
-	movies_2.date as remake_date,
-    m_abs.abstract 
-from movies movies_1
-JOIN movie_references m_ref ON movies_1.id = m_ref.referenced_id
-JOIN movies movies_2 ON movies_2.id = m_ref.movie_id
-JOIN movie_languages m_lang ON movies_1.id = m_lang.movie_id
-LEFT JOIN movie_abstracts_en m_abs ON movies_1.id = m_abs.movie_id
--- WHERE m2.type = 'Remake';
-WHERE m_ref.type = 'Remake' AND m_lang.language = 'en'
-ORDER BY original_movie_id;
-
-`;
 
 async function runOmdb() {
   await client.connect();
