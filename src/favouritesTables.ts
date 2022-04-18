ad7c121444f3a732effe2b0e4d9567923fedc01f
Nobeen <n.islam.1@outlook.com>
2022-04-18 11:32:35 +0100

    Created dummy favourites table

---
+++ src/favouritesTables.ts
@@ -0,0 +1,41 @@
+import { Client } from "pg"
+
+const dropFavourites = `
+    DROP TABLE IF EXISTS favourites
+`
+const createFavourites = `
+    CREATE TABLE favourites (
+        favourite_id INTEGER,
+        movie_id INTEGER,
+        PRIMARY KEY (favourite_id),
+        FOREIGN KEY (movie_id) REFERENCES movies (id)
+    );
+`
+const selectAllFavourites = `
+        SELECT * from favourites
+`
+const insertDummyFavourite = `
+        INSERT INTO favourites 
+        VALUES (1,1)
+`
+
+async function favouritesTable() {
+    const client = new Client({ database: "omdb" })
+    await client.connect()
+    console.log("You connected to the omdb database")
+    try {
+        await client.query(dropFavourites)
+        await client.query(createFavourites)
+        await client.query(insertDummyFavourite)
+        const res = await client.query(selectAllFavourites)
+        console.table(res.rows)
+    } catch (err) {
+        console.log(err.stack)
+    } finally {
+        client.end()
+        console.log("Thank you for joining!")
+    }
+
+}
+
+favouritesTable()
