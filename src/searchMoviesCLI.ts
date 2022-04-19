9313ad0d4bba72bd512b789a77b7c57881e7ab31
Nobeen <n.islam.1@outlook.com>
2022-04-19 16:26:47 +0100

    Removed unused queries. Paramaterised searchquery, quitClient and save sequence extracted as separate functions

--- src/searchMoviesCLI.ts
+++ src/searchMoviesCLI.ts
@@ -6,14 +6,46 @@ import { Client } from "pg";
 //we only need to specify the (non-default) database name.
 const client = new Client({ database: "omdb" });
 
-const selectAllFromMovies = `
-select * from movies
-limit 1
+const selectSearch = `
+SELECT id, name, date, runtime, budget, revenue, vote_average, votes_count, kind
+FROM movies
+WHERE 
+  lower(name) LIKE $1 AND
+  kind = 'movie' AND
+  date IS NOT null
+ORDER BY date DESC
+LIMIT 10
 `;
 
-const selectFavourites = `
-SELECT * from favourites
-`;
+async function quitClient() {
+  console.log("Sad to see you go!");
+  await client.end();
+}
+
+async function runSaveSequence(saveResultRows: any[]) {
+  const rowNumberToSave: string = question(
+    `Please input the row number of the movies you would like to save (0-9) / "q" to go back to search: `
+  );
+
+  if (rowNumberToSave === "q") {
+    return
+  } else {
+    const saveResult = saveResultRows[parseInt(rowNumberToSave)];
+    console.table([saveResult]);
+    let confirmSave = "";
+
+    while (isInputInvalid(confirmSave)) {
+      confirmSave = question(
+        `Are you sure you want to save this movie to favourites / y or n? `
+      );
+      if (confirmSave === "y") {
+        console.log("save");
+      } else if (confirmSave === "n") {
+        console.log("Save Canceled");
+      }
+    }
+  }
+}
 
 function isInputInvalid(confirmSave: string) {
   if (confirmSave === "y" || confirmSave === "n") {
@@ -23,6 +55,8 @@ function isInputInvalid(confirmSave: string) {
   }
 }
 
+
+
 async function runOmdb() {
   await client.connect();
   console.log("You have successfully connected to omdb database");
@@ -30,59 +64,26 @@ async function runOmdb() {
 
   let userSearch = "";
   while (userSearch !== "q") {
+
     userSearch = question(
       'Type to search for your movie / "q" to quit / "s" to see favourites: '
     ).toLowerCase();
 
-    const selectSearch = `
-    SELECT id, name, date, runtime, budget, revenue, vote_average, votes_count, kind
-    FROM movies
-    WHERE 
-      lower(name) LIKE '%${userSearch}%' AND
-      kind = 'movie' AND
-      date IS NOT null
-    ORDER BY date DESC
-    LIMIT 5
-  `;
-
     if (userSearch === "q") {
-      console.log("Sad to see you go!");
-      client.end();
-      return;
+      quitClient()
+      return
     }
 
     if (userSearch === "s") {
       console.log("Here are your favourites");
-      const favouritesRes = await client.query(selectFavourites);
+      const favouritesRes = await client.query(`SELECT * FROM favourites`);
       console.table(favouritesRes.rows);
     } else {
       try {
-        const searchRes = await client.query(selectSearch);
+        const searchRes = await client.query(selectSearch, [`%${userSearch}%`]);
         console.table(searchRes.rows);
-        const rowNumberToSave: string = question(
-          `Please input the row number of the movies you would like to save (0-9) / "q" to quit: `
-        );
 
-        if (rowNumberToSave === "q") {
-          console.log("Sad to see you go!");
-          client.end();
-          return;
-        } else {
-          const saveResult = searchRes.rows[parseInt(rowNumberToSave)];
-          console.table([saveResult]);
-          let confirmSave = "";
-
-          while (isInputInvalid(confirmSave)) {
-            confirmSave = question(
-              `Are you sure you want to save this movie to favourites / y or n? `
-            );
-            if (confirmSave === "y") {
-              console.log("save");
-            } else if (confirmSave === "n") {
-              console.log("Cancel");
-            }
-          }
-        }
+        runSaveSequence(searchRes.rows)
       } catch (err) {
         console.log(err.stack);
       }
